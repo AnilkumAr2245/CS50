c4273c5341ee30ddc732ae718902a08e652fc907
AnilkumAr2245 <tanneeruanilkumar02@gmail.com>
2026-01-19 15:12:26 +0530

    Add answer validation for the ultimate question

---
+++ deep/deep.py
@@ -0,0 +1,8 @@
+ans=input("What is the answer to Great Question of Life, the Universe and Everything?")
+ans=ans.lower()
+ans=ans.strip()
+if ans == "42" or ans == "forty-two" or ans == "forty two":
+    print("Yes")
+else:
+    print("No")
+

20c644fedcda36342eafd41e07615eb7a612384e
AnilkumAr2245 <tanneeruanilkumar02@gmail.com>
2026-01-30 00:23:24 +0000

    Add menu item input loop and running total calculation

---
+++ taqueria/taqueria.py
@@ -0,0 +1,33 @@
+menu={
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+money=0.0
+while True:
+    try:
+        item=input("Item: ").title()
+        if item in menu:
+            money+=menu[item]
+            print(f"${money:.2f}")
+    except EOFError:
+        print()
+        break
+
+
+
+
+
+
+
+
+
+
+
+
